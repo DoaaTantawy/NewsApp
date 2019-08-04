bb12a241910c0c5f8eecb34b5aa96d306b58c770
Doaa Tantawy <doaatantawy@192.168.116.128>
2019-08-04 04:59:29 -0700

    Initial Commit

---
+++ News/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  News
+//
+//  Created by Doaa Tantawy on 8/4/19.
+//  Copyright © 2019 Doaa Tantawy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
